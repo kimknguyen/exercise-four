7aaf3987f2d58abada05beec8470dffcab8f9ea6
Kim Nguyen <kkn2011@nyu.edu>
2023-11-09 08:43:12 -0500

    update

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express(); 
 const port = 4000; 
 
+const indexRoute = require("./routes/index"); 
+const aboutRoute = require("./routes/about"); 
+
 app.use(express.static("public")); 
+
+app.use('/', indexRoute); 
+app.use("/about", aboutRoute); 
 
 app.get('/', (req, res) => {
   res.send('Hello World!'); 
